97b8c37a510036b2c26a1e6dd3df96b1fa40ddf6
qeg1215 <kjhnml1234@naver.com>
2022-10-17 18:54:27 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HeaderLayout from '../views/layout/HeaderLayout.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/header',
+    name: 'header',
+    component: HeaderLayout
   },
   {
     path: '/about',
