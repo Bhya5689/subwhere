5348ccd17d318e211337828d7944ead6030d2cc8
seo <sit6518@gmail.com>
2022-10-18 11:31:26 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>2호선노선도</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="2500" height="1500" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
+        <g style="visibility: visible;">
+            <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
+        <path class="mw-link" d="M1280,70L1330,70" data-id="1838" data-type="link" data-line-id="1001" data-time="0" style="fill: none; stroke: rgb(20, 104, 190); stroke-width: 3;"></path>
     </div>
 </template>
-
+<script>
+export default {}
+</script>
