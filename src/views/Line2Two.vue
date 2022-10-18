aedd154abc2067e6e38377c52c386971dbb9db0c
seo <sit6518@gmail.com>
2022-10-18 11:22:22 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
         <h1>2호선노선도</h1>
-        <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-        <path>
-        </path>
-        </svg>
     </div>
 </template>
 
