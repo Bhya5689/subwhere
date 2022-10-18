563aca727e6559be1c4cbd063f55dbd3b4db92b2
seo <sit6518@gmail.com>
2022-10-18 11:35:14 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
         <h1>2호선노선도</h1>
-        <svg xmlns="http://www.w3.org/2000/svg" width="2500" height="1500">
-        <g style="visibility: visible;">
-            <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
-            </g>
-                <path class="mw-link" d="M1280,70L1330,70" data-id="1838" data-type="link" data-line-id="1001" data-time="0" style="fill: none; stroke: rgb(20, 104, 190); stroke-width: 3;"></path>
-        
     </div>
 </template>
 <script>
