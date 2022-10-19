c006084243815e1cb1a0003adf6e4a070b12ac9b
seointaek <sit6518@gmail.com>
2022-10-19 15:15:49 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -1,12 +1,12 @@
 <template>
    <div>
         <h1>2호선</h1>
-        <svg width="2500" height="1500" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
+        <svg xmlswidth="2500" height="1500" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
         <g style="visibility: visible;">
             <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
             <g>
-                 <path class="mw-link"  d="M1020,760L1020,800"  data-id="1" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(102, 204, 102); stroke-width: 3;"></path>
-                 <path class="mw-link"  d="M1020,800L1060,860"  data-id="2" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(102, 204, 102); stroke-width: 3;"></path>
+                 <path class="mw-link"  d="M1020,760L1020,800"  data-id="1" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(102, 204, 102); stroke-width: 30;"></path>
+                 <path class="mw-link"  d="M1020,800L1060,860"  data-id="2" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(102, 204, 102); stroke-width: 30;"></path>
             </g>
             </g>
         </svg>
