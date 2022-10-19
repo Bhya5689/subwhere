c97b0187785494c5e3ed2b4abfda16d248a60954
qeg1215 <kjhnml1234@naver.com>
2022-10-19 15:15:25 +0900

    line1

--- src/views/Line1One.vue
+++ src/views/Line1One.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>11호선</h1>
+        <h1>1호선</h1>
         <svg xmlns="http://www.w3.org/2000/svg" width="2500" height="1500" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
         <g style="visibility: visible;">
             <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
