00e8e3ab2e61d266bcd48cf1569262dec4583bb1
seo <sit6518@gmail.com>
2022-10-20 21:48:18 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -6,8 +6,8 @@
                 <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
                 <g>
                     <path class="mw-link" d="M526.5,680.5L726.5,680.5" data-id="27" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 30;"></path>
-                    <!-- <path class="mw-link" d="M666,731.5L666,605.5" data-id="1" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 50;"></path> -->
-                    <!--<path class="mw-link" d="M576.5,680.5L576.5,595.5" data-id="2" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0,  153, 0); stroke-width: 50;"></path>
+                    <path class="mw-link" d="M666,731.5L666,605.5" data-id="1" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 50;"></path>
+                    <path class="mw-link" d="M576.5,680.5L576.5,595.5" data-id="2" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0,  153, 0); stroke-width: 50;"></path>
                     <path class="mw-link" d="M1036.9,1000.5L1036.9,607.4" data-id="3" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0,  153, 0); stroke-width: 50;"></path>
                     <path class="mw-link" d="M960.9,553.5L960.9,562.5" data-id="4" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0,  153, 0); stroke-width: 5;"></path>
                     <path class="mw-link" d="M820.5,618L956.7,618" data-id="14" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
@@ -21,9 +21,9 @@
                     <path class="mw-link" d="M910.7,512.3L890.3,492" data-id="22" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
                     <path class="mw-link" d="M614.7,595.3L600.2,580.8" data-id="23" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
                     <path class="mw-link" d="M855.1,491.9L846.1,482.9" data-id="24" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
-                    <path class="mw-link" d="M669.5,501.7L886.5,501.7" data-id="25" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>-->
-                    <path class="mw-link" d="M914.5,557L986.9,557" data-id="26" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
-                    <path class="mw-link" d="M855.5,492L890,492" data-id="28" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 30;"></path>
+                    <path class="mw-link" d="M669.5,501.7L886.5,501.7" data-id="25" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
+                    <!--<path class="mw-link" d="M914.5,557L986.9,557" data-id="26" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 5;"></path>
+                    <path class="mw-link" d="M855.5,492L890,492" data-id="28" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(0, 153, 0); stroke-width: 30;"></path>-->
 
                 </g>
             </g>
