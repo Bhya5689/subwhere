9b6f14140aba6e6d275f1618864c38fd471a629a
seo <sit6518@gmail.com>
2022-10-20 20:50:29 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -4,19 +4,24 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="2500" height="1500" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
             <g style="visibility: visible;">
                 <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
-                <line class="st0" x1="732.5" y1="575.5" x2="671.5" y2="575.5"/>
-                <g>
-	                <line class="st1" x1="671.6" y1="560.4" x2="693.6" y2="582.4"/>
-                </g>
-                <g>
-                    <line class="st1" x1="707.5" y1="582.5" x2="694.5" y2="582.5"/>
-                </g>
-                <g>
-                    <line class="st1" x1="814.5" y1="582.5" x2="709.5" y2="582.5"/>
-                </g>
-                <g>
-                    <line class="st2" x1="727.5" y1="491.5" x2="753" y2="517"/>
-                </g>
+                <line class="st3" x1="139" y1="28" x2="139" y2="303.9"/>
+                <line class="st3" x1="139.3" y1="304.3" x2="161.5" y2="326.5"/>
+                <line class="st3" x1="288" y1="327" x2="336.5" y2="375.5"/>
+                <line class="st3" x1="461.1" y1="376.1" x2="616.3" y2="531.3"/>
+                <line class="st3" x1="692" y1="532" x2="732" y2="572"/>
+                <line class="st3" x1="813" y1="571.2" x2="836.9" y2="547.3"/>
+                <line class="st3" x1="986" y1="574" x2="1007.7" y2="552.3"/>
+                <line class="st3" x1="162.1" y1="326.5" x2="287.5" y2="326.5"/>
+                <line class="st3" x1="336.5" y1="375.5" x2="460.3" y2="375.5"/>
+                <line class="st3" x1="617" y1="531.5" x2="691.5" y2="531.5"/>
+                <line class="st0" x1="671.5" y1="575.5" x2="632.5" y2="536.5"/>
+                <line class="st3" x1="734.1" y1="572.5" x2="812" y2="572.5"/>
+                <line class="st3" x1="835.1" y1="545.5" x2="878.3" y2="545.5"/>
+                <line class="st3" x1="907.1" y1="574.5" x2="986" y2="574.5"/>
+                <line class="st3" x1="1007.7" y1="485.5" x2="1102" y2="485.5"/>
+                <line class="st3" x1="1141.3" y1="524.7" x2="1102" y2="485.5"/>
+                <line class="st3" x1="906.3" y1="573.7" x2="878.3" y2="545.7"/>
+                <line class="st3" x1="1007.7" y1="551" x2="1007.7" y2="485.9"/>
             </g>
         </svg>
     </div>
