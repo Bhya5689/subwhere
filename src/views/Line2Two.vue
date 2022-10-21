9ba420e47c982c53eee8f90606aa78821b02fcb0
seo <sit6518@gmail.com>
2022-10-21 22:26:33 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -49,6 +49,7 @@
                         <circle cx="3400" cy="1400 " r="30" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle>
                         <circle cx="3600" cy="1200 " r="20" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle>
                         <circle cx="3800" cy="1000 " r="30" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle>
+                        <circle cx="4000" cy="800 " r="20" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle>
                     </g>
                 </g>
             </g>
