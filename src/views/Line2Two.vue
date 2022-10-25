14972f8031424f83ae905fff478099d35cdfad06
seo <sit6518@gmail.com>
2022-10-25 21:08:10 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -5,7 +5,7 @@
             <g style="visibility: visible;">
                 <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
                 <g>
-                    <g class="mw-station" transform="translate(250, 240)" data-id="2778" data-type="station" data-line-id="1009" data-station-id="startpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(206, 164, 58); stroke: rgb(206, 164, 58); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">9호선</tspan></text></g>
+                    <g class="mw-station" transform="translate(600, 600)" data-id="2778" data-type="station" data-line-id="1002" data-station-id="startpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(45, 176, 61); stroke: rgb(45, 176, 61); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">2호선</tspan></text></g>
                 </g>
             </g>
         </svg>
