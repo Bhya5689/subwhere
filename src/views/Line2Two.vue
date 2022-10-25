18f4834e39ca8c18e3b53df9105837b18e237646
seo <sit6518@gmail.com>
2022-10-25 23:12:24 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -5,12 +5,13 @@
             <g style="visibility: visible;">
                 <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
                 <g>
-                    <g class="mw-station" transform="translate(600, 600)" data-id="2778" data-type="station" data-line-id="1002" data-station-id="startpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(45, 176, 61); stroke: rgb(45, 176, 61); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">2호선</tspan></text></g>
+                    <path class="mw-link" d="M680,670L615,670" data-id="2001" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(45, 176, 61); stroke-width: 20"></path>
+                    <path class="mw-link" d="M615,670L540,670" data-id="2001" data-type="link" data-line-id="1002" data-time="0" style="fill: none; stroke: rgb(45, 176, 61); stroke-width: 20"></path>
+                    <g class="mw-station" transform="translate(615, 670)" data-id="1" data-type="station" data-line-id="1002" data-station-id="" data-station-sub-id="" data-sibling="1931|290,1932|486" data-transfer-id="null" data-uptrain-pos="2063,1264,L" data-downtrain-pos="2063,1323,R"><circle r="12" style="fill: rgb(45, 176, 61); stroke: rgb(45, 176, 61);"></circle><text transform="rotate(-45)" style="text-anchor: middle; font-size: 14px; fill: rgb(102, 102, 102); display: initial;"><tspan x="40" y="-20">까치산</tspan></text></g>
+                    <g class="mw-station" transform="translate(680, 670)" data-id="2000" data-type="station" data-line-id="1002" data-station-id="startpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(45, 176, 61); stroke: rgb(45, 176, 61); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">2호선</tspan></text></g>
+                    <g class="mw-station" transform="translate(100, 90)" data-id="2000" data-type="station" data-line-id="1002" data-station-id="endpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(45, 176, 61); stroke: rgb(45, 176, 61); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">2호선</tspan></text></g>
                 </g>
             </g>
         </svg>
     </div>
 </template>
-<script>
-export default {}
-</script>
