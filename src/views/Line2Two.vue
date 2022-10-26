ca993095ed905330439610b6ac4165c6b368c3f3
seo <sit6518@gmail.com>
2022-10-26 13:38:09 +0900

    line2

--- src/views/Line2Two.vue
+++ src/views/Line2Two.vue
@@ -1,7 +1,7 @@
 <template>
    <div>
         <h1>2호선</h1>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1820" height="700" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1800" height="700" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">
             <g style="visibility: visible;">
                 <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
                 <g>
