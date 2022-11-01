65fbf68973924f89e13d8a29d1c67180dee790ab
seo <sit6518@gmail.com>
2022-11-01 20:52:28 +0900

    line7

--- src/views/Line7Seven.vue
+++ src/views/Line7Seven.vue
@@ -1,3 +1,18 @@
 <template>
     <h1>7호선</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1800" height="700" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red;" class="mw-subway">92
+            <g style="visibility: visible;">
+                <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                    <g class="mw-station" transform="translate(425, 100)" data-id="6000" data-type="station" data-line-id="1006" data-station-id="startpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(124, 137, 45); stroke: rgb(124, 137, 45); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">7호선</tspan></text></g>
+                    <g class="mw-station" transform="translate(1100, 570)" data-id="6000" data-type="station" data-line-id="1006" data-station-id="endpoint" data-station-sub-id="null" data-sibling="2806|1520" data-transfer-id="null" data-uptrain-pos="1197,26,R" data-downtrain-pos="1197,80,L"><rect width="50" height="31" x="-25" y="-18" style="rx: 4; fill: rgb(124, 137, 45); stroke: rgb(124, 137, 45); stroke-width: 3;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 16px; fill: rgb(255, 255, 255); font-weight: 900; pointer-events: none; display: initial;"><tspan x="0" y="4">7호선</tspan></text></g>
+                </g>
+                <g>
+                </g>
+            </g>
+    </svg>
 </template>
