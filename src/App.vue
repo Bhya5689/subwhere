82f8dec51387db4a39122f3c876818aa17194449
IN302 <kjhnml1234@gmail.com>
2022-11-08 13:00:50 +0900

    train

--- src/App.vue
+++ src/App.vue
@@ -73,4 +73,3 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
-
--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // build 경로 설정
-  outputDir: "../src/main/resources/static",
-  //indexPath : index.html 파일이 생성될 위치
-  indexPath: "../static/index.html",
-  //SpringBoot 서버 접속 주소
+  // outputDir: "../src/main/resources/static",
+  // //indexPath : index.html 파일이 생성될 위치
+  // indexPath: "../static/index.html",
+  // //SpringBoot 서버 접속 주소
   devServer: {
-    proxy: "http://localhost:8081"
+    proxy: "http://localhost:8080"
   },
   chainWebpack: config => {
     const svgRule = config.module.rule("svg");
