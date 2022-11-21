7e8ee8492cb3a04ce93cf07bc3fa3dcbd8416256
seo <sit6518@gmail.com>
2022-11-21 22:21:31 +0900

    line10

--- src/views/Line10Ten.vue
+++ src/views/Line10Ten.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-    <svg xmlns="http://www.w3.org/2000/svg" width="2000" height="1000" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red; margin: 0 auto" class="mw-subway">
+    <svg xmlns="http://www.w3.org/2000/svg" width="2000" height="1200" style="font-size: 12px; font-family: &quot;Nanum Gothic&quot;, sans-serif; font-weight: bold; letter-spacing: -1px; cursor: default; transform-origin: 0px 0px; transform: scale(0.8); fill: red; margin: 0 auto" class="mw-subway">
     <g style="visibility: visible;">
         <rect width="2500" height="1500" style="fill: rgb(255, 255, 255); opacity: 0.8;"></rect>
         <g>
-            <path class="mw-link" d="M1400,100L1480,100" data-id="1002" data-type="link" data-line-id="1001" data-time="0" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
-            <path class="mw-link" d="M1480,100L1560,100" data-id="1002" data-type="link" data-line-id="1001" data-time="0" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
+            <path class="mw-link" d="M880,150L1000,150" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
+            <path class="mw-link" d="M1000,150L1080,150" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
+            <path class="mw-link" d="M1080,150L1160,150" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
+            <path class="mw-link" d="M1160,150L1620,150" style="fill: none; stroke: rgb(38, 60, 150); stroke-width: 20"></path>
+            <g class="mw-station" transform="translate(1000, 150)"><circle r="15" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle><text transform="rotate(0)" style="text-anchor: middle; font-size: 20px; fill: rgb(102, 102, 102); display: initial;"><tspan x="" y="40">광운대</tspan></text></g>
+            <g class="mw-station" transform="translate(1160, 100)"><rect width="200" height="100" x="-22.5" y="-5" style="rx: 15; fill: rgb(255, 255, 255); stroke: rgb(38, 60, 150); stroke-width: 10;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 20px; fill: rgb(0, 0, 0); font-weight: 1500; pointer-events: none; display: initial;"><tspan x="75" y="50">월계</tspan></text></g>
+            <g class="mw-station" transform="translate(1500, 150)"><circle r="15" style="fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0);"></circle><text transform="rotate(0)" style="text-anchor: middle; font-size: 20px; fill: rgb(102, 102, 102); display: initial;"><tspan x="" y="40">녹천</tspan></text></g>
+            <g class="mw-station" transform="translate(920, 250)"><rect width="700" height="800" x="-22.5" y="-5" style="rx: 15; fill: rgb(255, 255, 255); stroke: rgb(0, 0, 0); stroke-width: 2;"></rect><text transform="rotate(0)" style="text-anchor: middle; font-size: 20px; fill: rgb(0, 0, 0); font-weight: 1500; pointer-events: none; display: initial;"><tspan x="75" y="50"></tspan></text></g>
         </g>
     </g>
 </svg>
